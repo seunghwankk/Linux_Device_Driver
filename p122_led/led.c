490736a508a21f9d47b98abbd62623ecf056e57d
seunghwankk <126065290+seunghwankk@users.noreply.github.com>
2023-05-12 09:55:32 +0900

    Update led.c

--- p122_led/led.c
+++ p122_led/led.c
@@ -7,37 +7,28 @@
 #define DEBUG 1
 #define IMX_GPIO_NR(bank, nr)       (((bank) - 1) * 32 + (nr))
 
-static int ledvalue = 15;
+static unsigned long ledvalue = 15;
+static char * twostring = NULL;
 
-module_param(ledvalue, int, 0);
+module_param(ledvalue, ulong ,0);
+module_param(twostring,charp,0);
 
-int led[] = {
+int led[4] = {
 	IMX_GPIO_NR(1, 16),   //16
 	IMX_GPIO_NR(1, 17),	  //17
 	IMX_GPIO_NR(1, 18),   //18
 	IMX_GPIO_NR(1, 19),   //19
 };
-
-static void led_write(unsigned long data)
-{
-	int i;
-	for(i = 0; i < ARRAY_SIZE(led); i++){
-		gpio_direction_output(led[i], (data >> i ) & 0x01);
-//		gpio_set_value(led[i], (data >> i ) & 0x01);
-	}
-#if DEBUG
-	printk("#### %s, data = %ld\n", __FUNCTION__, data);
-#endif
-}
-
 static int led_request(void)
 {
 	int ret = 0;
 	int i;
+
 	for (i = 0; i < ARRAY_SIZE(led); i++) {
 		ret = gpio_request(led[i], "gpio led");
 		if(ret<0){
 			printk("#### FAILED Request gpio %d. error : %d \n", led[i], ret);
+			break;
 		} 
 	}
 	return ret;
@@ -49,22 +40,73 @@ static void led_free(void)
 		gpio_free(led[i]);
 	}
 }
-static int led_init(void){
-	printk("Hello, world \n");
-	led_request();
+
+void led_write(unsigned long data)
+{
+	int i;
+	for(i = 0; i < ARRAY_SIZE(led); i++){
+//		gpio_direction_output(led[i], (data >> i ) & 0x01);
+//  	gpio_set_value(led[i], (data >> i ) & 0x01);
+  		gpio_direction_output(led[i], 0);
+    	gpio_set_value(led[i], (data >> i ) & 0x01);
+	}
+#if DEBUG
+	printk("#### %s, data = %ld\n", __FUNCTION__, data);
+#endif
+}
+void led_read(unsigned long * led_data)
+{
+	int i;
+	unsigned long data=0;
+	unsigned long temp;
+	for(i=0;i<4;i++)
+	{
+  		gpio_direction_input(led[i]); //error led all turn off
+		temp = gpio_get_value(led[i]) << i;
+		data |= temp;
+	}
+/*	
+	for(i=3;i>=0;i--)
+	{
+  		gpio_direction_input(led[i]); //error led all turn off
+		temp = gpio_get_value(led[i]);
+		data |= temp;
+		if(i==0)
+			break;
+		data <<= 1;  //data <<= 1;
+	}
+*/
+#if DEBUG
+	printk("#### %s, data = %ld\n", __FUNCTION__, data);
+#endif
+	*led_data = data;
+	return;
+}
+
+static int led_init(void)
+{
+	int ret
+	printk("Hello, world  [ledvalue=%ld:twostrig=%s] \n",ledvalue, twostring);
+	ret = led_request();
+	if(ret<0)
+	{
+//		return -EBUSY;  //Device or resource busy
+		return ret;       //Device or resource busy
+	}
 	led_write(ledvalue);
-	led_free();
 	return 0;
 }
 
-static void led_exit(void){
-	printk("Goodbye, world");
-	led_request();
+static void led_exit(void)
+{
+	printk("Goodbye, world \n");
 	led_write(0);
-	led_free();
+  	led_free();
 }
 
 module_init(led_init);
 module_exit(led_exit);
 
 MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("KSH");
+MODULE_DESCRIPTION("test module");
