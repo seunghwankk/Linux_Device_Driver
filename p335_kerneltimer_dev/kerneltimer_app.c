656cb1e0bea2ebae3354b2f989d2fd07e9ed6c4a
seunghwankk <126065290+seunghwankk@users.noreply.github.com>
2023-05-17 09:27:35 +0900

    Update kerneltimer_app.c

--- p335_kerneltimer_dev/kerneltimer_app.c
+++ p335_kerneltimer_dev/kerneltimer_app.c
@@ -9,14 +9,14 @@
 
 int main(int argc, char* argv[])
 {
-    int dev;
-//    char buff[128]={0};
-    int ret=0;
+	int dev;
+//  	char buff[128]={0};
+    	int ret=0;
 	int cnt = 0;
 	char key=0;
 	int old_key=0;
 
-    dev = open( DEVICE_FILENAME, O_RDWR|O_NDELAY );
+    	dev = open( DEVICE_FILENAME, O_RDWR|O_NDELAY );
 	if(dev < 0)
 	{
 		perror("open");
@@ -29,14 +29,14 @@ int main(int argc, char* argv[])
  			continue;
 		if(key != old_key)
 		{
-    		printf( "key = %d (%d)\n", key,cnt++);
-    		ret = write(dev,&key,sizeof(key));
+    			printf( "key = %d (%d)\n", key,cnt++);
+    			ret = write(dev,&key,sizeof(key));
 			if(key == 8)
 				break;
 			old_key = key;
 		}
 	}
 	close(dev);
-    return 0;
+    	return 0;
 }
 
