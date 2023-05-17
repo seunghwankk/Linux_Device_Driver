b58eeabe306d4539e1598f015e2510e412ebdd9d
tmdghks161952 <tmdghks161952@gmail.com>
2023-05-17 15:12:39 +0900

    update

---
+++ p369_ledkey_int/.ledkey_dev.mod.o.d
@@ -0,0 +1,64 @@
+ledkey_dev.mod.o: /home/udooer/dd/p369_ledkey_int/ledkey_dev.mod.c \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/linux/kconfig.h \
+ include/generated/autoconf.h include/linux/module.h include/linux/list.h \
+ include/linux/types.h include/uapi/linux/types.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h include/uapi/asm-generic/int-ll64.h \
+ arch/arm/include/generated/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h include/uapi/asm-generic/bitsperlong.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/linux/posix_types.h \
+ include/linux/stddef.h include/uapi/linux/stddef.h \
+ include/linux/compiler.h include/linux/compiler-gcc.h \
+ include/linux/compiler-gcc4.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/posix_types.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/asm-generic/posix_types.h \
+ include/linux/poison.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/linux/const.h \
+ include/linux/stat.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/stat.h \
+ include/uapi/linux/stat.h include/linux/time.h include/linux/cache.h \
+ include/uapi/linux/kernel.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/linux/sysinfo.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/cache.h \
+ include/linux/seqlock.h include/linux/spinlock.h \
+ include/linux/typecheck.h include/linux/preempt.h \
+ include/linux/linkage.h include/linux/stringify.h include/linux/export.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/linkage.h \
+ arch/arm/include/generated/asm/preempt.h include/asm-generic/preempt.h \
+ include/linux/thread_info.h include/linux/bug.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/bug.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/opcodes.h \
+ include/linux/swab.h include/uapi/linux/swab.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/swab.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/swab.h \
+ include/asm-generic/bug.h include/linux/kernel.h \
+ /usr/lib/gcc/arm-linux-gnueabihf/4.8/include/stdarg.h \
+ include/linux/bitops.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/bitops.h \
+ include/linux/irqflags.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/irqflags.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/ptrace.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/ptrace.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/hwcap.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/hwcap.h \
+ include/asm-generic/bitops/non-atomic.h \
+ include/asm-generic/bitops/fls64.h include/asm-generic/bitops/sched.h \
+ include/asm-generic/bitops/hweight.h \
+ include/asm-generic/bitops/arch_hweight.h \
+ include/asm-generic/bitops/const_hweight.h \
+ include/asm-generic/bitops/lock.h include/asm-generic/bitops/le.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/uapi/asm/byteorder.h \
+ include/linux/byteorder/little_endian.h \
+ include/uapi/linux/byteorder/little_endian.h \
+ include/linux/byteorder/generic.h \
+ include/asm-generic/bitops/ext2-atomic-setbit.h include/linux/log2.h \
+ include/linux/printk.h include/linux/init.h include/linux/kern_levels.h \
+ include/linux/dynamic_debug.h include/linux/string.h \
+ include/uapi/linux/string.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/string.h \
+ include/linux/errno.h include/uapi/linux/errno.h \
+ arch/arm/include/generated/asm/errno.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/asm-generic/errno.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/include/uapi/asm-generic/errno-base.h \
+ /home/udooer/kernel/linux_kernel-3.14-1.0.x-udoo/arch/arm/include/asm/div64.h \
+ /home/udooer/kernel/linux
