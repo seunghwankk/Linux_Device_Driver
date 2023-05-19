1f8e249edd3d501fa54f9b666bbb10b0198c4f0b
seunghwankk <126065290+seunghwankk@users.noreply.github.com>
2023-05-19 09:31:39 +0900

    Update ioctl_test.h

--- test/ioctl_test.h
+++ test/ioctl_test.h
@@ -1,16 +1,14 @@
 #ifndef __IOCTL_H__
 #define __IOCTL_H__
 
-#define IOCTLTEST_MAGIC 't'
+#define MAGIC 't'
 typedef struct
 {
-	unsigned long size;
-	unsigned char buff[128];
-	int timer_val;
+	unsigned int timer_val;
 } __attribute__((packed)) ioctl_test_info;
 
-#define TIMER_START	 			_IO(TIMER_MAGIC, 0) 
-#define TIMER_STOP			 	_IO(TIMER_MAGIC, 1)
-#define TIMER_VALUE				_IOW(TIMER_MAGIC, 2,keyled_data)
-#define TIMER_MAXNR				8
+#define TIMER_VALUE				_IOW(TIMER_MAGIC, 0,keyled_data)
+#define TIMER_START	 			_IO(TIMER_MAGIC, 1) 
+#define TIMER_STOP			 	_IO(TIMER_MAGIC, 2)
+#define TIMER_MAXNR				3
 #endif
